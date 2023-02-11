7a07ccf1418bfea51c6526ac136bbbc0d8598b2d
amitmahlawat <amitmahlawat3@gmail.com>
2023-02-11 21:30:25 +0530

    user data restored

--- src/Components/UpdateProfile.js
+++ src/Components/UpdateProfile.js
@@ -1,18 +1,53 @@
 import userEvent from "@testing-library/user-event";
-import React,{useRef} from "react";
-import { Form,Card, Container,Button } from "react-bootstrap";
+import React,{useEffect, useRef, useState} from "react";
+import { Form,Card, Container,Button, Navbar } from "react-bootstrap";
 
 
 const UpdateProfile=()=>{
+const[completed,SetCompleted]=useState(0)
+const[nameUpdate,SetnameUpdate]=useState(false)
+const[UrlUpdate,SetUrlUpdate]=useState(false)
+
 const NameRef=useRef();
 const urlRef=useRef();
+
+
+
+const nameChangeHandler=()=>{
+    if(NameRef.current.value){
+        SetnameUpdate(true)
+    }else{
+        SetnameUpdate(false)
+    }
+}
+
+const UrlChangeHandler=()=>{
+    if(urlRef.current.value){
+        SetUrlUpdate(true)
+    }else{
+        SetUrlUpdate(false)
+    }
+}
+useEffect(()=>{
+    if(nameUpdate && UrlUpdate){
+        SetCompleted(100)
+    }else if(nameUpdate || UrlUpdate){
+        SetCompleted(50)
+    }else{
+        SetCompleted(0)
+    }
+
+})
+    
+
+
 
     const SubmitHandler=(e)=>{
         e.preventDefault();
         const enteredName=NameRef.current.value;
         const enteredURL=urlRef.current.value;
         const token=localStorage.getItem('token')
-        
+
         fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCjIdx0_Dp6Zypz0RoEoDqh8A81TIyYtzE',
         {
             method:'POST',
@@ -29,21 +64,55 @@ const urlRef=useRef();
         }).then(res=>{
             res.json().then(data=>{
                 console.log(data)
+                
+                
+            })
+        })       
+    }
+    
+    
+    useEffect(()=>{
+        const token=localStorage.getItem('token')
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCjIdx0_Dp6Zypz0RoEoDqh8A81TIyYtzE',{
+           method:'POST', 
+        body:JSON.stringify({
+                idToken:token
+            }),
+            headers:{
+                'Content-Type':'/application/jason'
+            }
+            }).then(res=>{
+            res.json().then(data=>{
+                NameRef.current.value=data.users[0].displayName
+                SetnameUpdate(true)
+                console.log(data.users[0])
+                urlRef.current.value=data.users[0].photoUrl
+                SetUrlUpdate(true)
             })
         })
-    }
+    },[])
 
 
     return(<>
         <Container>
+        
+        <Navbar bg='dark' variant="dark">
+
+        
+        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>your profile is {completed}% complete</Navbar.Text>
+        </Navbar.Collapse>
+
+        
+        </Navbar    >
         <h1 style={{textAlign:'center'}}>Contact Details</h1>
         <Card>
             <Card.Body>
         <Form onSubmit={SubmitHandler}>
             <Form.Label htmlFor="fullname">Full Name</Form.Label>
-            <Form.Control id='fullname' type="text" ref={NameRef}></Form.Control>
+            <Form.Control id='fullname' type="text" onChange={nameChangeHandler} ref={NameRef} required></Form.Control>
             <Form.Label htmlFor="url">Profile photo URL</Form.Label>
-            <Form.Control id='url' type="url" ref={urlRef}></Form.Control>
+            <Form.Control id='url' type="url" onChange={UrlChangeHandler} ref={urlRef} required></Form.Control>
             <Button  className="mt-3" type="submit" >update</Button>
         </Form>
         </Card.Body>
